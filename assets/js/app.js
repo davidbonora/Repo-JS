f2e73ae41286b3e1f31ab744544004f6ba6cb119
alimarchi <marchi_alice@yahoo.com>
2022-10-14 18:07:55 +0200

    Algorithms 2 ej. 2

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Algorithms 2 - Ejercicio 2
+// Escribe un programa que muestre las letras del palíndromo "no lemon, no melon" en orden inverso.
+
+function ejercicio1_2() {
+    let text = "no lemon, no melon";
+    let letters = [...text];
+    let palindrome = letters.reverse();
+    return console.log(palindrome);
+}
+
+ejercicio1_2()
+
+// Algorithms 2 - Ejercicio 6
+// Escribe un programa para generar estadísticas de texto.
+
