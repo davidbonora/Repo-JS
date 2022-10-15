122309b117b47edfe1e2d6b5845fa9bf16eacbad
Dani <jdvillalba2@hotmail.com>
2022-10-15 11:17:07 +0200

    solucionando problema

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,26 +4,26 @@
 let split = palindrome.split("")
 console.log(split);*/
 
-///////// Algorithms 2 - Ej 7 ////////////
+///////// Algorithms 2 - Ej 7 ////////////  
 
-/*let numbers = [0,1,2,3,4,5,6,7,8,9,10]
-let min = 0
-let max = numbers.length -1
-number = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(numbers[number]);
-//prompt("Please, guest the number")
-while (condition) {
-    
-}*/
-    
 let min = 0
 let max = 10
 let number = Math.floor(Math.random()* (max - min +1)) + min;
-let tries = 3
+let tries = 1
 let answer = prompt("Please, guest the number")
 console.log(number)
-if (answer == number) {
- alert("You Win!")   
-} else if (answer != number) {
-  alert("Try it again")  
+
+while (answer != number && tries < 3) {
+  //prompt("Please, guest the number");
+  tries++;
+  if (answer != number) {
+    alert("Try it again");
+    prompt("Please, guest the number")
+  } if (answer == number) {
+    alert("You Win!");
+  }
+  else if (tries == 3 && answer != number) {
+    alert("Sorry, Good luck in love");
+  }
 }
+
